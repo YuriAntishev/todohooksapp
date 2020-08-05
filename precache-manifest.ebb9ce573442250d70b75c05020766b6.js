d4f5a54ac88c4f05536a369792563edc572c7db0
Antishev Yuri <ant_ustas@mail.ru>
2020-08-05 23:57:42 +0300

    Updates

---
+++ precache-manifest.ebb9ce573442250d70b75c05020766b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1a1491e523e572ccd26d66a59afe71",
+    "url": "/todohooksapp/index.html"
+  },
+  {
+    "revision": "d193170c251c81772271",
+    "url": "/todohooksapp/static/js/2.a0b74ccf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todohooksapp/static/js/2.a0b74ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6da9648cf4be3f6b5154",
+    "url": "/todohooksapp/static/js/main.218d59dd.chunk.js"
+  },
+  {
+    "revision": "e71f0b41a1764be429fe",
+    "url": "/todohooksapp/static/js/runtime-main.c8006a35.js"
+  }
+]);
